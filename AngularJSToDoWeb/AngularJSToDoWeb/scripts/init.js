0eb9b2518469e5579a4d9e7de11eb0853a9e3c1d
Dr. Doris Chen <doristchen@hotmail.com>
2015-09-13 13:06:03 -0700

    delete Angular app

--- AngularJSToDoWeb/AngularJSToDoWeb/scripts/init.js
+++
@@ -1,7 +0,0 @@
-﻿(function () {
-    'use strict';
-    angular.module('xPlat', ['xPlat.services', 'xPlat.controllers', 'xPlat.directives']);
-    angular.module('xPlat.directives', []);
-    angular.module('xPlat.controllers', []);
-    angular.module('xPlat.services', ['ngResource']);
-})();
